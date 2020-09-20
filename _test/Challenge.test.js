49b1887244678a689b10b2b747f4c2c3e1e4322b
Dhakal555 <aayushdhakal544@gmail.com>
2020-09-20 07:36:18 +0545

    test

---
+++ _test/Challenge.test.js
@@ -0,0 +1,9 @@
+const axios = require("axios");
+const baseurl = "http://localhost:3002/user";
+
+describe("challenge route test", () => {
+    let token;
+    test("add a new challenge", () => {
+        return axios
+    })
+});
