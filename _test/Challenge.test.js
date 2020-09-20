8a360dea4687a38b0c8404e075cf991bb7bde03f
Dhakal555 <aayushdhakal544@gmail.com>
2020-09-20 08:30:24 +0545

    challengetest

--- _test/Challenge.test.js
+++ _test/Challenge.test.js
@@ -1,9 +1,45 @@
 const axios = require("axios");
-const baseurl = "http://localhost:3002/user";
+const baseurl = "http://localhost:3002/challenge";
 
 describe("challenge route test", () => {
     let token;
     test("add a new challenge", () => {
         return axios
+        .post(baseurl + "/", {
+            chType: "solo",
+            chGame: "Mario",
+            chAmt: "100"
+        })
     })
+
+    test("get all challenges", () => {
+        return axios
+        .get(baseurl + "/", {
+        })
+        .then(response => {
+            expect(response.status).toBe(200);
+        });
+    })
+
+    test("get all challenges", () => {
+        return axios
+        .get(baseurl + "/5f66b760e4d8c911d0f1dcae", {
+        })
+        .then(response => {
+            expect(response.status).toBe(200);
+        });
+    })
+
+    test("get all challenges", () => {
+        return axios
+        .put(baseurl + "/5f66b760e4d8c911d0f1dcae", {
+            chType: "solo",
+            chGame: "MarioBros",
+            chAmt: "100"
+        })
+        .then(response => {
+            expect(response.status).toBe(200);
+        });
+    })
+
 });
